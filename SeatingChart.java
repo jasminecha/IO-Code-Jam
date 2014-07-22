a6a357f46424ad6071164a9fc5d44d549764d83a
Jasmine Cha <jasminecha@users.noreply.github.com>
2014-07-21 21:52:46 -0400

    Created SeatingChart.java
    This code currently does not run and did not complete the third task in the Code Jam challenge. I'm working on fixing it so it does.

---
+++ SeatingChart.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SeatingChart {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("A-small-attempt0.in");
+		Scanner keyboard = new Scanner(file);
+
+		int number = keyboard.nextInt();
+		ArrayList<String> array = new ArrayList<String>(number);
+		keyboard.nextLine();
+		for (int i = 0; i < number; i++) {
+			array.add(keyboard.nextLine());
+		}
+
+		int possibleCombo = 0;
+		for (int i = 0; i < array.size(); i++) {
+			String ara = array.get(i);
+			int n = Integer.parseInt(ara.substring(0, ara.indexOf(" ")));
+			ara = ara.substring(ara.indexOf(" "));
+			int k = Integer.parseInt(ara.substring(0, ara.indexOf(" ")));
+			System.out.println("Case #" + (i + 1) + ": " + possibleCombo);
+		}
+	}
+}
